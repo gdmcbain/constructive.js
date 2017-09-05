e8900f1b6c7df982352984af240eab4f2fe21bba
G. D. McBain <gdmcbain@protonmail.com>
2017-09-05 22:02:10 +1000

    switched to arrow-function style

--- peano.js
+++ peano.js
@@ -1,23 +1,17 @@
 // Copyright (C) 2017 G. D. McBain
 
-zero = {pred: undefined};
+"use strict";
 
-function isZero(n) {
-    return n.pred == undefined;
-}
+const zero = {pred: undefined};
 
-function toNumber(n) {
-    return isZero(n) ? 0 : 1 + toNumber(n.pred);
-}
+const isZero = n => n.pred == undefined;
 
-function succ(n) {
-    return {pred: n};
-}
+const toNumber = n => isZero(n) ? 0 : 1 + toNumber(n.pred);
 
-function add(n, m) {
-    return isZero(m) ? n : add(succ(n), m.pred);
-}
+const succ = n => ({pred: n});
+
+const add = n => m => isZero(m) ? n : add(succ(n)) (m.pred);
 
 console.log(toNumber(zero));
 console.log(toNumber(succ(zero)));
-console.log(toNumber(add(zero, succ(zero))));
+console.log(toNumber(add(zero) (succ(zero))));
