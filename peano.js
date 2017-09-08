149d117180114b731548532df1b198a44106a9d1
G. D. McBain <gmcbain@memjet.com>
2017-09-08 10:24:44 +1000

    required ramda

--- peano.js
+++ peano.js
@@ -2,16 +2,27 @@
 
 "use strict";
 
+const R = require('ramda');
+
 const zero = {};
 
 const succ = n => ({pred: n});
 
-const isZero = n => n.pred == undefined;
+const pred = R.prop('pred')
 
-const toNumber = n => isZero(n) ? 0 : 1 + toNumber(n.pred);
+const isNonzero = R.has('pred');
 
-const add = n => m => isZero(m) ? n : add(succ(n)) (m.pred);
+const isZero = R.complement(isNonzero);
+
+const toNumber = n => isZero(n) ? 0 : R.pipe(pred, toNumber, R.inc)(n);
+
+const add = m => isZero(m) ? R.identity : R.pipe(succ, add(pred(m)));
+
+const mul = m => isZero(m) ? R.always(zero) : n => add (n) (mul(pred(m))(n));
 
 console.log(toNumber(zero));
 console.log(toNumber(succ(zero)));
-console.log(toNumber(add(zero) (succ(zero))));
+console.log(toNumber(add(zero)(succ(zero))));
+console.log(toNumber(add(succ(zero))(zero)));
+console.log(toNumber(mul(zero)(succ(zero))));
+console.log(toNumber(mul(succ(zero))(zero)));
