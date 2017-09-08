7ce423c8fb367362618ed9ddeab8372eb8469bfc
G. D. McBain <gmcbain@memjet.com>
2017-09-08 11:20:37 +1000

    added from Number

--- peano.js
+++ peano.js
@@ -20,13 +20,15 @@ const isZero = R.complement(isNonzero);
 
 const toNumber = n => isZero(n) ? 0 : R.pipe(pred, toNumber, R.inc)(n);
 
+const fromNumber = n => n == 0 ? zero : R.pipe(R.dec, fromNumber, succ)(n);
+
 const add = m => isZero(m) ? R.identity : R.pipe(succ, add(pred(m)));
 
 const mul = m => isZero(m) ? R.always(zero) : n => add (n) (mul(pred(m))(n));
 
 const factorial = n => isZero(n) ? one : mul (n) (factorial(pred(n)));
-      
 
+      
 console.log(toNumber(zero));
 console.log(toNumber(one));
 console.log(toNumber(add(zero)(one)));
@@ -34,3 +36,5 @@ console.log(toNumber(add(one)(zero)));
 console.log(toNumber(mul(zero)(one)));
 console.log(toNumber(mul(one)(zero)));
 console.log(toNumber(factorial(three)));
+console.log(toNumber(mul(fromNumber(3))(fromNumber(5))));
+
